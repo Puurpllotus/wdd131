a2e26aa111c5863b1eff252783a654a79e2f1de6
puurpllotus <tlongjohn@byupathway.edu>
2025-09-11 12:23:27 +0100

    week01-Homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+function updateDates() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    const lastModifiedDate = document.lastModified;
+    document.getElementById('lastModified').textContent = `Last Modification: ${lastModifiedDate}`;
+}
+
+document.addEventListener('DOMContentLoaded', updateDates);
